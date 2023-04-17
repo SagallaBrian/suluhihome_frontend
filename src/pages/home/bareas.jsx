d09755fbb603aa63f994cc791fdbff195d56d516
Brian Sagalla <brayozwebdesigner@gmail.com>
2023-04-17 14:17:04 +0300

    testimonials

--- src/pages/home/bareas.jsx
+++ src/pages/home/bareas.jsx
@@ -11,25 +11,28 @@ import Col from "react-bootstrap/Col";
 const sulservice = [
   {
     id: 1,
-    icons: "MdOutlineBlurOn",
+    icons: MdOutlineLaptop,
     headers: "Web application development",
     descrip: `  From idea to launch, we create web applications 
     that are tailored to your business needs. Let us give your business a digital presence`,
   },
   {
     id: 2,
+    icons: MdMemory,
     headers: "Content management systems",
     descrip: `  Take control of your website content with our
      easy-to-use and flexible content management systems`,
   },
   {
     id: 3,
+    icons: MdOutlineShoppingCart,
     headers: "E-commerce solutions",
     descrip: ` Simplify your online sales process and grow your 
     business with our secure and easy-to-use e-commerce solutions`,
   },
   {
     id: 4,
+    icons: MdOutlineBlurOn,
     headers: "UX/ UI Design",
     descrip: ` Create engaging and intuitive web experiences that 
     keep your customers coming back for more 
@@ -38,24 +41,18 @@ const sulservice = [
 ];
 
 const Bareas = () => {
-  const myservices = sulservice.map((oneItem) => (
-    <Col lg key={oneItem.id}>
-      <div className="p-3 bg-white mb-3">
-        {oneItem.id === 1 && (
-          <MdOutlineLaptop className="display-6 text-info" />
-        )}
-        {oneItem.id === 2 && <MdMemory className="display-6 text-info" />}
-        {oneItem.id === 3 && (
-          <MdOutlineShoppingCart className="display-6 text-info" />
-        )}
-        {oneItem.id === 4 && (
-          <MdOutlineBlurOn className="display-6 text-info" />
-        )}
-        <h5 className="roboto fw-normal">{oneItem.headers}</h5>
-        <p className="p_fonts text-secondary">{oneItem.descrip}</p>
-      </div>
-    </Col>
-  ));
+  const myservices = sulservice.map((oneItem) => {
+    const Icon = oneItem.icons;
+    return (
+      <Col lg key={oneItem.id}>
+        <div className="p-3 bg-white mb-3">
+          <Icon className="display-6 text-info" />
+          <h5 className="roboto fw-normal">{oneItem.headers}</h5>
+          <p className="p_fonts text-secondary">{oneItem.descrip}</p>
+        </div>
+      </Col>
+    );
+  });
 
   return (
     <div className="bg-light roboto">
--- src/pages/home/gtestimonial.jsx
+++ src/pages/home/gtestimonial.jsx
@@ -3,134 +3,97 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { MdGrade } from "react-icons/md";
 
-import csa from "../../assets/cs1.webp";
+import avatar from "../../assets/avatar.jpg";
+
+const testim = [
+  {
+    id: 1,
+    ratin: 4,
+    name: "Pradnya Lane",
+    company: "Aneka Labs",
+    remarks: `Suluhi Technology helped us create a custom 
+    web application that completely transformed the 
+    way we do business. They were professional, 
+    responsive, and delivered on time and on budget`,
+  },
+  {
+    id: 2,
+    ratin: 4,
+    name: "Kevin Kamotho",
+    company: "Kirubi Stores",
+    remarks: `We worked with Suluhi to develop 
+    our website, and we couldn't be happier with the results. 
+    They listened to our needs and provided us with a solution that is 
+    both user-friendly and visually stunning`,
+  },
+  {
+    id: 3,
+    ratin: 4,
+    name: "Humphrey Ochieng",
+    company: "Betika",
+    remarks: `Suluhi Technology's website maintenance and support 
+    services have been a lifesaver for our business. Their team is 
+    always quick to respond and resolve any issues we encounter, 
+    and their proactive approach to website management has 
+    saved us time and money`,
+  },
+  {
+    id: 4,
+    ratin: 4,
+    name: "Peter Korir",
+    company: "Carens",
+    remarks: `Suluhi Technology's website design and development 
+    services completely exceeded our expectations. They took the 
+    time to understand our brand and our target audience, and created a website 
+    that perfectly represents our business`,
+  },
+];
 
 const Testimonial = () => {
+  const mytestimonials = testim.map((oneMonial) => (
+    <Col lg key={oneMonial.id}>
+      <div className="p-3 py-4 bg-white rounded shadow mb-3">
+        <div className="sul-theme-green">
+          {[...Array(oneMonial.ratin)].map((_, i) => (
+            <MdGrade key={i} />
+          ))}
+        </div>
+        <div className="text-secondary small py-3">{oneMonial.remarks}</div>
+
+        <div className="d-flex align-items-center py-2">
+          <div className="pe-2">
+            <img src={avatar} alt="" className="customer-image" />
+          </div>
+          <div className="small lh-sm ">
+            <div className="sul-theme-blue">{oneMonial.name}</div>
+            <div className="sul-theme-green">{oneMonial.company}</div>
+          </div>
+        </div>
+      </div>
+    </Col>
+  ));
   return (
     <div className="testimo-split">
       <Container fluid>
         <div className="py-5">
           <div className="py-3">
             <Row>
-              <Col md></Col>
-              <Col md>
+              <Col lg className="d-none d-xl-block"></Col>
+              <Col lg>
                 <div className="text-white">
                   <div className="small">Testimonials</div>
                   <h4 className="sul-semibold">What the Clients Say </h4>
                 </div>
               </Col>
-              <Col md></Col>
-              <Col md></Col>
-              <Col md></Col>
+              <Col lg></Col>
+              <Col lg></Col>
+              <Col lg></Col>
             </Row>
           </div>
 
           <Row>
-            <Col md></Col>
-            <Col md>
-              <div className="p-3 py-4 bg-white rounded shadow">
-                <div className="sul-theme-green">
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                </div>
-                <div className="text-secondary small py-3">
-                  One of the main reasons we switched to Suluhi is how easy it
-                  is to use, and even if you get stuck, their customer care team
-                  will get you back on track in minutes
-                </div>
-
-                <div className="d-flex align-items-center py-2">
-                  <div className="pe-2">
-                    <img src={csa} alt="" className="customer-image" />
-                  </div>
-                  <div className="small lh-sm ">
-                    <div className="sul-theme-blue">Pradnya Dexlab</div>
-                    <div className="sul-theme-green">Managing Director</div>
-                  </div>
-                </div>
-              </div>
-            </Col>
-            <Col md>
-              <div className="p-3 py-4 bg-white rounded shadow">
-                <div className="sul-theme-green">
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                </div>
-                <div className="text-secondary small py-3">
-                  One of the main reasons we switched to Suluhi is how easy it
-                  is to use, and even if you get stuck, their customer care team
-                  will get you back on track in minutes
-                </div>
-
-                <div className="d-flex align-items-center py-2">
-                  <div className="pe-2">
-                    <img src={csa} alt="" className="customer-image" />
-                  </div>
-                  <div className="small lh-sm ">
-                    <div className="sul-theme-blue">Pradnya Dexlab</div>
-                    <div className="sul-theme-green">Managing Director</div>
-                  </div>
-                </div>
-              </div>
-            </Col>
-            <Col md>
-              <div className="p-3 py-4 bg-white rounded shadow">
-                <div className="sul-theme-green">
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                </div>
-                <div className="text-secondary small py-3">
-                  One of the main reasons we switched to Suluhi is how easy it
-                  is to use, and even if you get stuck, their customer care team
-                  will get you back on track in minutes
-                </div>
-
-                <div className="d-flex align-items-center py-2">
-                  <div className="pe-2">
-                    <img src={csa} alt="" className="customer-image" />
-                  </div>
-                  <div className="small lh-sm ">
-                    <div className="sul-theme-blue">Pradnya Dexlab</div>
-                    <div className="sul-theme-green">Managing Director</div>
-                  </div>
-                </div>
-              </div>
-            </Col>
-            <Col md>
-              <div className="p-3 py-4 bg-white rounded shadow">
-                <div className="sul-theme-green">
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                  <MdGrade />
-                </div>
-                <div className="text-secondary small py-3">
-                  One of the main reasons we switched to Suluhi is how easy it
-                  is to use, and even if you get stuck, their customer care team
-                  will get you back on track in minutes
-                </div>
-
-                <div className="d-flex align-items-center py-2">
-                  <div className="pe-2">
-                    <img src={csa} alt="" className="customer-image" />
-                  </div>
-                  <div className="small lh-sm ">
-                    <div className="sul-theme-blue">Pradnya Dexlab</div>
-                    <div className="sul-theme-green">Managing Director</div>
-                  </div>
-                </div>
-              </div>
-            </Col>
+            <Col lg className="d-none d-xl-block"></Col>
+            {mytestimonials}
           </Row>
           <div className="py-4"></div>
         </div>
