03a60f6f545be97c34b27644b5f0b2a99ef5a228
Brian Sagalla <brayozwebdesigner@gmail.com>
2023-04-26 05:00:03 +0300

    porfolio links

--- src/pages/home/components/eportfolio.jsx
+++ src/pages/home/components/eportfolio.jsx
@@ -3,50 +3,60 @@ import { Col, Container, Row } from "react-bootstrap";
 const portfolios = [
   {
     id: 1,
-    category: "CMS",
-    productn: "Suluhi Accounting",
+    category: "MIS",
+    productn: "Accounting Software",
     imgbackg: "portfo1",
+    links: "https://accounts-suluhi.onrender.com/",
   },
   {
     id: 2,
-    category: "CMS",
-    productn: "Chicken Farm",
-    imgbackg: "portfo2",
+    category: "MIS",
+    productn: "Poultry Management",
+    imgbackg: "portfo3",
+    links: "https://poultry-suluhi.onrender.com/",
   },
   {
     id: 3,
     category: "E-commerce",
     productn: "Suluhi Market",
-    imgbackg: "portfo3",
+    imgbackg: "portfo2",
+    links: "https://commerce-suluhi.onrender.com/",
   },
-  {
-    id: 4,
-    category: "UX Design",
-    productn: "General Motors",
-    imgbackg: "portfo4",
-  },
-  {
-    id: 5,
-    category: "UX Design",
-    productn: "Pharmaceutical",
-    imgbackg: "portfo5",
-  },
-  {
-    id: 6,
-    category: "",
-    productn: "",
-    imgbackg: "portfo6",
-  },
+  // {
+  //   id: 4,
+  //   category: "UX Design",
+  //   productn: "General Motors",
+  //   imgbackg: "portfo4",
+  // },
+  // {
+  //   id: 5,
+  //   category: "UX Design",
+  //   productn: "Pharmaceutical",
+  //   imgbackg: "portfo5",
+  // },
+  // {
+  //   id: 6,
+  //   category: "",
+  //   productn: "",
+  //   imgbackg: "portfo6",
+  // },
 ];
 const PortfolioSection = () => {
-  const renderedportfolio = portfolios.map((onePort) => (
-    <Col md={4} key={onePort.id}>
-      <div className={`${onePort.imgbackg} mb-3`}>
-        <div className="innerPort p-3">
-          <div className="sul-theme-green">{onePort.category}</div>
-          <h5 className="fw-normal py-0 my-0">{onePort.productn}</h5>
+  const renderedportfolio = portfolios.map((onePort, ind) => (
+    <Col md={4} key={ind}>
+      <a
+        href={onePort.links}
+        target={"_blank"}
+        rel="noreferrer"
+        className="text-light"
+      >
+        <div className={`${onePort.imgbackg} mb-3`}>
+          <div className="innerPort p-3">
+            <div className="sul-theme-green">{onePort.category}</div>
+            <h5 className="fw-normal py-0 my-0">{onePort.productn}</h5>
+          </div>
         </div>
-      </div>
+      </a>
     </Col>
   ));
   return (
